0e0f331bc03a0513fe5990443151c53adce3a344
sayra17 <agussayra03@gmail.com>
2022-02-06 15:51:58 +0700

    Initial commit

---
+++ tugas30.js
@@ -0,0 +1,7 @@
+function regex(value) {
+    var ambildata = value.match(/[es]/g)
+    console.log(ambildata)
+}
+regex(
+    "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat"
+);
